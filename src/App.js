b082d0bee272e597859ed89274f44b822df4ad63
Williams Ramirez <willicastilloo@gmail.com>
2019-03-01 17:31:17 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
+import ProductList from "./Components/ProductList";
+import Cart from "./Components/Cart";
+import Details from "./Components/Details";
+import Default from "./Components/Default";
+import Modal from "./Components/Modal";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Modal />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
